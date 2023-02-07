698acd6bf1772418f69d5606525d29a39f45655a
Ryan C <ryan2.crawley@live.uwe.ac.uk>
2023-02-07 12:53:44 +0000

    All elements working as intended

--- C++ Assignment 2/whack-a-mole-22-23-konane-z/src/Game.h
+++ C++ Assignment 2/whack-a-mole-22-23-konane-z/src/Game.h
@@ -6,37 +6,39 @@
 
 class Game
 {
- public:
-  Game(sf::RenderWindow& window);
-  ~Game();
-  bool init();
-  void update(float dt);
-  void render();
-  void mouseClicked(sf::Event event);
-  void keyPressed(sf::Event event);
+public:
+    Game(sf::RenderWindow& window);
+    ~Game();
+    bool init();
+    void update(float dt);
+    void render();
+    void mouseClicked(sf::Event event);
+    void keyPressed(sf::Event event);
 
- private:
-  bool collisionCheck(sf::Vector2i click, sf::Sprite sprite);
-  void spawn();
+private:
+    bool collisionCheck(sf::Vector2i click, sf::Sprite sprite);
+    void spawn();
 
-  sf::RenderWindow& window;
-  sf::Sprite background;
-  sf::Texture background_texture;
-  sf::Sprite bird;
-  sf::Texture bird_texture;
-  sf::Font font;
-  sf::Text title_text;
+    sf::RenderWindow& window;
+    sf::Sprite background;
+    sf::Texture background_texture;
+    sf::Sprite bird;
+    sf::Texture bird_texture;
+    sf::Font font;
+    sf::Text title_text;
 
-  bool in_menu;
-  sf::Text menu_text;
-  sf::Text play_option;
-  sf::Text quit_option;
-  bool play_selected = true;
+    bool in_menu;
+    sf::Text menu_text;
+    sf::Text play_option;
+    sf::Text quit_option;
+    sf::Text score_text;
+    sf::Text score_text_header;
+    bool play_selected = true;
 
-  bool reverse = false;
-  float speed = 200;
+    bool reverse = false;
+    float speed = 200;
 
-  int score = 0;
+    int score = 0;
 
 };
 
